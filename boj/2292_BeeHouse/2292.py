3fb05ea18794c00058e68100d333d95b7180d35c
seungcholcho <seung.chol.cho@gmail.com>
2022-09-26 11:19:18 +0900

    26SEP22 boj 2292

---
+++ boj/2292_BeeHouse/2292.py
@@ -0,0 +1,6 @@
+n=int(input())
+r,l=0,1
+while n>l:
+        r+=1
+        l+=r*6
+print(r+1)
--- leetcode/test_space.py
+++ leetcode/test_space.py
@@ -1,5 +1,9 @@
-B = "Hello"
-print('H' in B)
-print('c' in B)
-
-# checking git
+n=66
+r,l=1,7
+while True:
+    if n <= l:
+        print(r+1)
+        break
+    elif n > l:
+        r+=1
+        l = l + r * 6
