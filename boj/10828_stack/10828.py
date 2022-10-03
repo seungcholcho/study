7f0393491dbb1e0e3b5d35e4886e932a37c2278f
seungcholcho <seung.chol.cho@gmail.com>
2022-10-03 21:22:40 +0900

    03OCT22 BlackJack

--- boj/10828_stack/10828.py
+++ boj/10828_stack/10828.py
@@ -1,57 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-class Stack:
-    def __init__(self) -> None:
-        self.size = -1 
-        self.top = -1
-        self.stack = []
-
-    def push(self, num):
-        self.size += 1
-        self.top += 1
-        self.stack.append(num)
-
-    def pop(self):
-        if self.size == -1:
-            print("-1")
-        else:
-            print(self.stack[self.top])
-            del self.stack[self.top]
-            self.top -= 1
-            self.size -= 1 
-
-    def size(self):
-        print(self.size+1)
-
-    def empty(self):
-        if self.size== -1:
-            print('1')
-        else:
-            print('0')
-
-    def topp(self):
-        if self.size == -1:
-            print("-1")
-        else:
-            print(self.stack[self.top])
-
-stack = Stack()
-
 n = int(input())
+stack = []
 
-command = input().split()
-
-print(command)
-for cmd in command:
-    if "push" in cmd:
-        cmd = cmd.strip("push ")
-        stack.push(cmd)
-    elif cmd == "pop":
-        stack.pop()
-    elif cmd == "size":
-        stack.size()
-    elif cmd == "empty":
-        stack.empty()
-    elif cmd == "top":
-        stack.topp()
+for i in range(n):
+    s = input().split()
+    if s[0] == 'push':
+        stack.append(int(s[1]))
+    elif s[0] == 'top':
+        if len(stack) > 0: print(stack[-1])
+        else: print(-1)
+    elif s[0] == 'pop':
+        if len(stack) > 0: print(stack.pop())
+        else: print(-1)
+    elif s[0] == 'size':
+        print(len(stack))
+    elif s[0] == 'empty':
+        if len(stack) > 0: print(0)
+        else: print(1)
---
+++ boj/2798_BlackJack/2798.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+s = 0
+c = []
+n = input().split()
+p = int(n[0])
+b = int(n[1])
+n = input().split()
+for i in range(0,p):
+    c.append(int(n[i]))
+for i in range(0,len(c)-2):
+    for j in range(i+1,len(c)-1):
+        for k in range(j+1,len(c)):
+            t = c[i]+c[j]+c[k]
+            if(b >= t > s):
+                s = t
+print(s)
